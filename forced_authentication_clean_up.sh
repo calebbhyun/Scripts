e541b325891e0121724f06ea87c2ac6991c1b6f1
Caleb <54153064+calebbhyun@users.noreply.github.com>
2024-07-24 22:14:10 -0300

    Created forced_authentication_clean_up.sh

---
+++ forced_authentication_clean_up.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Author: Caleb Bhyun
+
+# Create a list of shares in a format like:
+# \\HOST\SHARE1
+# \\HOST\SHARE2
+# ...
+
+# ./forced_authentication_clean_up.sh 'FQDN/USERNAME' 'PASSWORD' FILENAME
+
+# Check if username, password, and filename were provided
+if [ $# -ne 3 ]; then
+    echo "Incorrect number of arguments provided. Usage: ./test.sh USERNAME PASSWORD FILENAME"
+    exit 1
+fi
+
+# Define the username and password
+username="$1"
+password="'$2'"
+
+# Define the commands to run after connecting
+cmds="
+rm @test.scf
+rm @test.url
+rm @test.library-ms
+"
+
+# Read each line from the input file
+while IFS= read -r line
+do
+  # Replace single backslashes with double backslashes
+  line=${line//\\/\\\\}
+
+  # Add double quotes around the line
+  line="\"$line\""
+
+  # Define the command to connect to the SMB share
+  cmd="smbclient -U $username%$password $line"
+
+  # Pass the commands to smbclient
+  echo "$cmds" | eval $cmd
+done < "$3"
